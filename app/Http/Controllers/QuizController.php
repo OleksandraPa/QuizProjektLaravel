d0fd56fcd0e32d638a8a54432ac3fbc13cc0e36a
Oleksandra Panshyna <sashun.ka06@gmail.com>
2025-12-10 00:09:20 +0100

    dodawanie danych z opcjami wyboru i lista quizow

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -6,57 +6,78 @@ use Illuminate\Http\Request;
 
 class QuizController extends Controller
 {
+    //dane z opcjami wyboru
     private $quizzes = [
         1 => [
             'title' => 'Quiz Matematyczny: Podstawowe Działania',
-            'description' => 'Test z dodawania, odejmowania i mnożenia.',
+            'description' => 'Test wiedzy z dodawania, odejmowania i mnożenia w formie testu.',
             'questions' => [
-                1 => ['text' => 'Ile to jest $7 + 15$?', 'answer' => '22'],
-                2 => ['text' => 'Jaki jest wynik działania $8 \times 6$?', 'answer' => '48'],
-                3 => ['text' => 'Ile wynosi $\sqrt{49}$?', 'answer' => '7'],
+                1 => [
+                    'text' => 'Ile to jest 7 + 15?', 
+                    'options' => [
+                        'A' => '21',
+                        'B' => '22',
+                        'C' => '23',
+                        'D' => '24',
+                    ],
+                    'answer' => 'B' 
+                ],
+                2 => [
+                    'text' => 'Jaki jest wynik działania 8 × 6?', 
+                    'options' => [
+                        'A' => '42',
+                        'B' => '48',
+                        'C' => '54',
+                        'D' => '60',
+                    ],
+                    'answer' => 'B' 
+                ],
+                3 => [
+                    'text' => 'Ile wynosi pierwiastek kwadratowy z 49?', 
+                    'options' => [
+                        'A' => '6',
+                        'B' => '7',
+                        'C' => '8',
+                        'D' => '9',
+                    ],
+                    'answer' => 'B' 
+                ],
             ]
         ],
         2 => [
-            'title' => 'Angielski/Polski: Zwierzęta (Animals)',
-            'description' => 'Sprawdź, czy znasz polskie odpowiedniki popularnych zwierząt.',
+            'title' => 'Tłumaczenie z angielskiego na polski: Zwierzęta',
+            'description' => 'Wybierz polski odpowiednik popularnych zwierząt.',
             'questions' => [
-                1 => ['text' => 'Jakie polskie słowo oznacza **Dog**?', 'answer' => 'Pies'],
-                2 => ['text' => 'Jakie polskie słowo oznacza **Cat**?', 'answer' => 'Kot'],
-                3 => ['text' => 'Jakie polskie słowo oznacza **Elephant**?', 'answer' => 'Słoń'],
+                1 => [
+                    'text' => 'Jakie polskie słowo oznacza "Dog"?', 
+                    'options' => [
+                        'A' => 'Koń',
+                        'B' => 'Pies',
+                        'C' => 'Krowa',
+                        'D' => 'Królik',
+                    ],
+                    'answer' => 'B'
+                ],
+                2 => [
+                    'text' => 'Jakie polskie słowo oznacza "Elephant"?', 
+                    'options' => [
+                        'A' => 'Żyrafa',
+                        'B' => 'Lew',
+                        'C' => 'Słoń',
+                        'D' => 'Nosorożec',
+                    ],
+                    'answer' => 'C'
+                ],
             ]
         ]
     ];
 
+    //lista quizow
     public function index()
     {
         return view('quizzes.index', ['quizzes' => $this->quizzes]);
     }
 
-    public function show($id)
-    {
-        if (!isset($this->quizzes[$id])) {
-            abort(404);
-        }
-        $quiz = $this->quizzes[$id];
-        $quiz['id'] = $id;
-
-        return view('quizzes.show', ['quiz' => $quiz]);
-    }
-
-    public function question($quizId, $questionId)
-    {
-        if (!isset($this->quizzes[$quizId]) || !isset($this->quizzes[$quizId]['questions'][$questionId])) {
-            abort(404);
-        }
-
-        $quiz = $this->quizzes[$quizId];
-        $question = $quiz['questions'][$questionId];
-
-        return view('quizzes.question', [
-            'quizTitle' => $quiz['title'],
-            'quizId' => $quizId,
-            'questionId' => $questionId,
-            'questionText' => $question['text']
-        ]);
-    }
+    
+    
 }
