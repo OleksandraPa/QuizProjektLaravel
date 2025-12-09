071fa32d6032af3b2bf93c2fe6872e1b1c39f86d
Oleksandra Panshyna <sashun.ka06@gmail.com>
2025-12-10 00:58:23 +0100

    skonfigurowano modele quiz i question

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Question extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['quiz_id', 'text', 'options', 'answer'];
+    
+    protected $casts = [
+        'options' => 'array',
+    ];
+
+    public function quiz(): BelongsTo
+    {
+        return $this->belongsTo(Quiz::class);
+    }
 }
