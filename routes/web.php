efe4045c48cd3e2d949944a08ad52c2a96cdbf4e
Oleksandra Panshyna <sashun.ka06@gmail.com>
2025-12-10 00:15:28 +0100

    web trasa do głównej strony

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\QuizController; 
 
-// Trasa dla strony głównej (/)
+// trasa dla strony głównej
 Route::get('/', function () {
-    return '<h1>Witamy na Stronie Głównej QuizProject!</h1><p>Przejdź do /quizzes, aby zobaczyć listę.</p>';
+    return view('home'); 
 })->name('home');
-
-// Trasa dla listy quizów (/quizzes)
-Route::get('/quizzes', function () {
-    $quizzes = [
-        "Quiz z PHP",
-        "Wprowadzenie do Laravela",
-        "Podstawy baz danych MariaDB",
-        "Test wiedzy ogólnej"
-    ];
-
-    $output = '<h1>Lista Dostępnych Quizów</h1><ul>';
-    foreach ($quizzes as $quiz) {
-        $output .= '<li>' . htmlspecialchars($quiz) . '</li>';
-    }
-    $output .= '</ul>';
-
-    return $output;
-})->name('quizzes.index');
