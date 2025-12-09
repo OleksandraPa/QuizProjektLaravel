716cdf762e51922a898d0da53c163a080917e5f5
Oleksandra Panshyna <sashun.ka06@gmail.com>
2025-12-10 00:35:46 +0100

    dodawanie wszystkich tras do web

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,13 @@ Route::prefix('quizzes')->group(function () {
     // Lista quizów
     Route::get('/', [QuizController::class, 'index'])->name('quizzes.index');
 
+    // Szczegóły pojedynczego quizu
+    Route::get('/{id}', [QuizController::class, 'show'])->name('quizzes.show');
+
+    // Pojedyncze pytanie (GET: wyświetlenie)
+    Route::get('/{quizId}/question/{questionId}', [QuizController::class, 'question'])->name('quizzes.question');
+
+    // Przesłanie odpowiedzi (POST: sprawdzenie)
+    Route::post('/{quizId}/question/{questionId}', [QuizController::class, 'submitAnswer'])->name('quizzes.submit');
+    
 });
